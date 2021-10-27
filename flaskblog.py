830814c4f0b453f071f3376698acd4342873f674
Bibek Rauth <rauth.bibek007@gmail.com>
2021-10-27 23:51:07 +0530

    render template added to serve

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 '''
 Instance of Flask class will be our WSGI application. 
@@ -13,11 +13,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
