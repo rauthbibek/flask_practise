c85cd497f1856760be38d2d673388ee7ecdc8ea7
Bibek Rauth <rauth.bibek007@gmail.com>
2021-10-27 23:32:14 +0530

    added python main file run option

--- flaskblog.py
+++ flaskblog.py
@@ -11,5 +11,14 @@ app = Flask(__name__)
 @app.route("/") is a decorator which adds  extra functionalities to the existing function.
 '''
 @app.route("/")
+@app.route("/home")
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About page</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
