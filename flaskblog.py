8434740614f02167bd62c4c090be5d97de4b4494
Bibek Rauth <rauth.bibek007@gmail.com>
2021-10-28 00:17:47 +0530

    added dynamic values to display

--- flaskblog.py
+++ flaskblog.py
@@ -7,17 +7,32 @@ Python sets the __name__ variable to the module name, so the value of this varia
 
 app = Flask(__name__) 
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 '''
 @app.route("/") is a decorator which adds  extra functionalities to the existing function.
 '''
 @app.route("/")
 @app.route("/home")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 if __name__ == '__main__':
